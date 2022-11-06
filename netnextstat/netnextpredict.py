041add064c999f3e8e56749c9b502328465e2f6a
Steen Hegelund <steen@hegelund-hansen.dk>
2022-11-06 21:54:09 +0100

    get linux version tags from a linux git repo and add to output

--- netnextstat/netnextpredict.py
+++ netnextstat/netnextpredict.py
@@ -13,6 +13,8 @@ import argparse
 import datetime
 import yaml
 import sys
+import subprocess
+import re
 
 import jinja2
 
@@ -31,19 +33,27 @@ class NetNextCycle:
         self.current = False
         self.predicted = False
         self.update()
+        self.version = None
 
     def update(self):
         today = datetime.date.today()
         if self.day1 <= today and today <= self.day3:
             self.current = True
 
+    def set_version(self, version):
+        self.version = version
+
+    def no_version(self):
+        return self.version == None
+
     def __str__(self):
-        return 'Open: {} to {}, {} days, Closed: {} to {}, {} days'.format(self.day1,
-                                                                           self.day2 - datetime.timedelta(days=1),
-                                                                           self.open.days,
-                                                                           self.day2,
-                                                                           self.day3 - datetime.timedelta(days=1),
-                                                                           self.close.days)
+        return 'Open: {} to {}, {} days, Closed: {} to {}, {} days, {}'.format(self.day1,
+                                                                               self.day2 - datetime.timedelta(days=1),
+                                                                               self.open.days,
+                                                                               self.day2,
+                                                                               self.day3 - datetime.timedelta(days=1),
+                                                                               self.close.days,
+                                                                               self.version)
 
 class PredictedNetNextCycle(NetNextCycle):
     def __init__(self, day1, open_days, closed_days):
@@ -55,6 +65,29 @@ class PredictedNetNextCycle(NetNextCycle):
         self.predicted = False
         self.update()
         self.predicted = True
+        self.version = None
+
+class LinuxTag:
+    regex = re.compile(r'Linux (\d+)\.(\d+)')
+    def __init__(self, date, tag, version):
+        self.date = datetime.date(date.year, date.month, date.day)
+        self.tag = tag
+        self.version = version
+
+    def increment(self):
+        mt = LinuxTag.regex.findall(self.version)
+        if mt:
+            major = int(mt[0][0])
+            minor = int(mt[0][1])
+            if minor >= 19:
+                major += 1
+                minor = 0
+            else:
+                minor += 1
+            self.version = f'Linux {major}.{minor}'
+
+    def __str__(self):
+        return f'{self.date}: {self.version}'
 
 def generate_history(data):
     laststate = None
@@ -86,18 +119,52 @@ def predict(cycles):
         cycles.append(PredictedNetNextCycle(open_date, next_open, next_closed))
     return cycles
 
-def generate_html(cycles, outputpath):
+
+def git_tag_history(lines):
+    history = []
+    regex = re.compile(r'([^;]+)-rc1;([^;]+);([^;]+)-rc1')
+    for line in lines:
+        mt = regex.findall(line)
+        if mt:
+            tagstr, datestr, versionstr = mt[0][0:3]
+            history.append(LinuxTag(datetime.datetime.fromisoformat(datestr), tagstr, versionstr))
+    return history
+
+def get_git_tags(repo):
+    cp = subprocess.run(['git', '-C', repo, 'tag', '-l',
+                         '--format=%(refname:short);%(taggerdate:iso-strict);%(contents:subject)',
+                         '--sort=taggerdate'],
+                        capture_output=True)
+    if cp.returncode == 0:
+        return git_tag_history(cp.stdout.decode().split('\n'))
+    return None
+
+def add_linux_versions(cycles, linux_versions):
+    for cycle in cycles:
+        for tag in linux_versions:
+            diff = tag.date - cycle.day3
+            if diff.days <= 2:
+                cycle.set_version(tag.version)
+
+    last_tag = linux_versions[-1]
+    for cycle in cycles:
+        if cycle.predicted:
+            last_tag.increment()
+            cycle.set_version(last_tag.version)
+
+def generate_html(cycles, linux_versions, outputpath):
     html_filename = os.path.join(os.path.expanduser(outputpath), 'netnext.html')
     environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
     html_template = environment.get_template("netnext.html.jinja")
     with open(html_filename, mode="w", encoding="utf-8") as results:
-        results.write(html_template.render({'cycles': cycles }))
+        results.write(html_template.render({'cycles': cycles, 'linux_versions': linux_versions }))
         print(f"... wrote {html_filename}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-g', '--generate', help='Generate an HTML file with the prediction', action='store_true')
     parser.add_argument('-o', '--outdir', help='Output folder for generated files', type=str, default='.')
+    parser.add_argument('-r', '--repo', help='path to linux git repo with tag information', type=str, default=None)
     parser.add_argument('filename', help='Path to the netnext datastore file', type=str, metavar='path',
                         default=statuspath)
     args = parser.parse_args()
@@ -106,11 +173,17 @@ if __name__ == '__main__':
         if not os.path.exists(absfilename):
             print('No netnext status file found at {}'.format(absfilename))
             sys.exit(1)
+
+        linux_versions = None
+        if args.repo:
+            linux_versions = get_git_tags(os.path.expanduser(args.repo))
+
         data = load_datastore(absfilename)
         cycles = generate_history(data)
         cycles = predict(cycles)
+        add_linux_versions(cycles, linux_versions)
         if args.generate:
-            generate_html(cycles, args.outdir)
+            generate_html(cycles, linux_versions, args.outdir)
         else:
             for cycle in cycles:
                 print(cycle)
